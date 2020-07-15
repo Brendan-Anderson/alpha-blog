cd0734e8bf80c6e035cb00d5db491da8fc94a905
Brendan Anderson <brendan.anderson1323@gmail.com>
2020-07-14 22:44:52 -0700

    create new article page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,19 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      redirect_to @article
+    else
+      render 'new'
+    end
+
+  end
+
 end
