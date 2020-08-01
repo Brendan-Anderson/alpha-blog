b1956a0ff59def49295104178a14e2df5296b6a2
Brendan-Anderson <Brendan-Anderson@users.noreply.github.com>
2020-08-01 02:27:16 -0700

    Finished category index and show views. Added edit and form partial for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,12 +5,14 @@ class CategoriesController < ApplicationController
     @category = Category.new
   end
 
+
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
   end
 
   def show
     @category = Category.find(params[:id])
+    @articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
   
   def create
@@ -20,6 +22,20 @@ class CategoriesController < ApplicationController
       redirect_to @category
     else
       render 'new'
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = "Category name updated successfully"
+      redirect_to @category
+    else
+      render 'edit'
     end
   end
 
