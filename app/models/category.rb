5f28b05eef46210b4805d8e2221463117a3d965b
Brendan-Anderson <Brendan-Anderson@users.noreply.github.com>
2020-08-01 00:01:26 -0700

    Created many-to-many association for article category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
   validates :name, presence: true, length: { minimum: 3, maximum: 25}
   validates_uniqueness_of :name
+  has many :article_categories
+  has many :articles, through: :article_categories
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_31_020017) do
+ActiveRecord::Schema.define(version: 2020_08_01_060715) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
